105dfb025103bc66a92997be8142c52f666ed302
Leonilian <jacobwilliams.tampa@gmail.com>
2024-03-09 14:34:18 -0500

    added count_letters function

--- main.py
+++ main.py
@@ -11,4 +11,21 @@ def count_words(filepath):
         counter +=1
     return counter
 
+def count_letters(filepath):
+    content = get_contents(filepath)
+    lettercounts = {}
+    lowercontent=content.lower()
+    for word in lowercontent:
+        for letter in word:
+            if letter.isalpha():
+                if letter in lettercounts:
+                    lettercounts[letter] += 1
+                else:
+                    lettercounts[letter] = 1
+    sorted_letters = dict(sorted(lettercounts.items()))
+    return sorted_letters
+                    
+
+
 print(count_words("books/frankenstein.txt"))
+print(count_letters("books/frankenstein.txt"))
